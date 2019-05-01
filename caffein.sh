fb2fa192553f91ff19583cea703f668bc36a169d
ssgalitsky <sgalitsky@gmail.com>
2019-05-01 16:04:16 +0300

    Update caffein.sh

--- caffein.sh
+++ caffein.sh
@@ -28,11 +28,17 @@ CAFFE_ROOT=/opt/caffe
 # https://github.com/docker/hub-feedback/issues/460
 CLONE_TAG=1.0
 
-git clone -b ${CLONE_TAG} --depth 1 https://github.com/BVLC/caffe.git . && \
-    pip install --upgrade pip && \
-    cd python && for req in $(cat requirements.txt) pydot; do pip install $req; done && cd .. && \
-    git clone https://github.com/NVIDIA/nccl.git && cd nccl && make -j install && cd .. && rm -rf nccl && \
-    mkdir build && cd build && \
-    cmake -DUSE_CUDNN=1 -DUSE_NCCL=1 .. && \
-    make -j"$(nproc)"
+git clone -b ${CLONE_TAG} --depth 1 https://github.com/BVLC/caffe.git .
+pip install --upgrade pip 
+cd python 
+for req in $(cat requirements.txt) pydot; do pip install $req; done 
+cd .. 
+git clone https://github.com/NVIDIA/nccl.git 
+cd nccl 
+make -j install 
+cd .. 
+rm -rf nccl 
+mkdir build && cd build 
+cmake -DUSE_CUDNN=1 -DUSE_NCCL=1 .. 
+make -j"$(nproc)"
  
